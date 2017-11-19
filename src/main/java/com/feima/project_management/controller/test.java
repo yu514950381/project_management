2296e0126da63c73247361107de43e643ebe3e51
yu514950381 <514950381@qq.com>
2017-11-19 20:50:12 +0800

    整理了一下资源

--- src/main/java/com/feima/project_management/controller/test.java
+++ src/main/java/com/feima/project_management/controller/test.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/test1")
+@RequestMapping("/jump")
 public class test {
 
     @RequestMapping("/test2")
     public String hello(){
         return "index";
+    }
+
+    @RequestMapping("/memberManagement")
+    public String memberManagement(){
+        return "member_management";
     }
 
 }
