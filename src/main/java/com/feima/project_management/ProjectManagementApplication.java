495debfd9838fd07edf9f69c3a0f1bda50b5ff5e
1574070120 <1574070120@qq.com>
2017-11-21 21:27:38 +0800

    美化处理

--- src/main/java/com/feima/project_management/ProjectManagementApplication.java
+++ src/main/java/com/feima/project_management/ProjectManagementApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 @SpringBootApplication
 @MapperScan("com.feima.project_management.dao")
 public class ProjectManagementApplication {
