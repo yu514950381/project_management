b2309fcd12b1764beaabacaa9cbe15ad6d7d0289
yu514950381 <514950381@qq.com>
2018-01-11 01:46:30 +0800

    基本完成了对登录页面的前后天编写

---
+++ src/main/java/com/feima/project_management/dao/LoginAndRegisterMapper.java
@@ -0,0 +1,17 @@
+package com.feima.project_management.dao;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+/**
+ * @author 喻世琦
+ * 该类的作用是对应登录和注册时对数据库需要进行的操作
+ * 对应的mapper为loginAndRegisterMapper.xml
+ * */
+@Repository("/loginAndRegisterMapper")
+public interface LoginAndRegisterMapper {
+
+    Map loginSelect(String loginName);
+
+}
