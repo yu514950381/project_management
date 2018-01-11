333cc5599f905f7d47c72457054c0629eef81740
xieguolv <2189843632@qq.com>
2018-01-11 23:38:17 +0800

    0v0

--- src/main/java/com/feima/project_management/dao/MissionManagementMapper.java
+++ src/main/java/com/feima/project_management/dao/MissionManagementMapper.java
@@ -1,8 +1,14 @@
 package com.feima.project_management.dao;
 
+import com.feima.project_management.model.Mission;
 import org.springframework.stereotype.Repository;
 
 @Repository("missionManagementMapper")
 public interface MissionManagementMapper {
+
+    void missionInsert(Mission mission);
+
+    void missionDeleteByPrimaryKey(Integer missionId);
+
 
 }
